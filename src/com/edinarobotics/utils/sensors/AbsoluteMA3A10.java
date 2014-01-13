ee3c224b6477e4118b997942226daf367e4408db
Karl Otness <karl@otness.com>
2014-01-13 16:22:51 -0600

    Add methods to AnalogAbsoluteEncoder and its subclasses.
    The new methods allow AnalogAbsoluteEncoder to be easily zeroed and
    fix rollover detection in the encoder.

--- src/com/edinarobotics/utils/sensors/AbsoluteMA3A10.java
+++ src/com/edinarobotics/utils/sensors/AbsoluteMA3A10.java
@@ -1,11 +1,12 @@
 package com.edinarobotics.utils.sensors;
 
 import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.AnalogTrigger;
 
 /**
  * Implements an analog absolute encoder class for US Digital's
  * analog (A10) model of the MA3 series of absolute encoders.
- * These encoder read from 0V-5V over a range of 0 - 360 degrees.
+ * These encoders read from 0V-5V over a range of 0 - 360 degrees.
  */
 public class AbsoluteMA3A10 extends AnalogAbsoluteEncoder{
     
@@ -16,8 +17,11 @@ public class AbsoluteMA3A10 extends AnalogAbsoluteEncoder{
      * is connected.
      * @param angleOffset The angle offset to be applied in degrees. The angle
      * offset is <i>added</i> to the returned angle.
+     * @param reverseReadDirection If {@code true} the encoder will have its
+     * increasing angle direction reversed. The rotation direction that would
+     * normally count up will instead count down.
      */
-    public AbsoluteMA3A10(AnalogChannel analogChannel, double angleOffset){
-        super(analogChannel, 0.0, 5.0, angleOffset);
+    public AbsoluteMA3A10(AnalogChannel analogChannel, double angleOffset, boolean reverseReadDirection){
+        super(analogChannel, 0.0, 5.0, angleOffset, reverseReadDirection);
     }
 }
--- src/com/edinarobotics/utils/sensors/AnalogAbsoluteEncoder.java
+++ src/com/edinarobotics/utils/sensors/AnalogAbsoluteEncoder.java
@@ -1,16 +1,18 @@
 package com.edinarobotics.utils.sensors;
 
 import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.PIDSource;
 
 /**
  * Implements a general analog absolute encoder. This class simplifies the
  * task of working with absolute encoders by performing all required voltage
  * calculations and returning a simple angle.
  */
-public class AnalogAbsoluteEncoder {
+public class AnalogAbsoluteEncoder implements PIDSource{
     private AnalogChannel analogChannel;
-    private double minVolts, maxVolts, angleOffset;
-    private boolean reversed;
+    private double minVolts, maxVolts, angleOffset, lastAngle, zeroingOffset;
+    private boolean reversed, reverseReadDirection;
+    private static final double MAX_PRE_READ_DISTANCE = 180.0;
     
     /**
      * Constructs a new AnalogAbsoluteEncoder given the analog channel to which
@@ -19,10 +21,12 @@ public class AnalogAbsoluteEncoder {
      * @param analogChannel The analog channel of the absolute encoder.
      * @param voltage0Deg The voltage of the encoder at 0 degrees.
      * @param voltage360Deg The voltage of the encoder at 360 degrees.
-     * @param angleOffset The angle offset to be applied in degrees. The angle
+     * @param zeroingOffset The angle offset to be applied in degrees. The angle
      * offset is <i>added</i> to the returned angle.
+     * @param reverseReadDirection If {@code true} the encoder will read in the
+     * opposite direction (the angle will be subtracted from 360 degrees).
      */
-    public AnalogAbsoluteEncoder(AnalogChannel analogChannel, double voltage0Deg, double voltage360Deg, double angleOffset){
+    public AnalogAbsoluteEncoder(AnalogChannel analogChannel, double voltage0Deg, double voltage360Deg, double zeroingOffset, boolean reverseReadDirection){
         this.analogChannel = analogChannel;
         minVolts = voltage0Deg;
         maxVolts = voltage360Deg;
@@ -33,13 +37,14 @@ public class AnalogAbsoluteEncoder {
             maxVolts = temp;
             reversed = true;
         }
-        this.angleOffset = angleOffset;
+        this.zeroingOffset = zeroingOffset;
+        this.reverseReadDirection = reverseReadDirection;
     }
     
     /**
      * Returns the raw voltage read from the analog encoder directly from the
      * analog channel.
-     * @return The rav voltage (in volts) as read from the analog channel.
+     * @return The raw voltage (in volts) as read from the analog channel.
      */
     public double getVoltage(){
         return analogChannel.getVoltage();
@@ -49,8 +54,8 @@ public class AnalogAbsoluteEncoder {
      * Returns the angle read from the analog encoder in radians. This angle is
      * computed based on the voltages given to the constructor of
      * AnalogAbsoluteEncoder.
-     * @return The angle read from the analog encoder in radians. The angle will
-     * always be in the range 0 to 2*pi.
+     * @return The angle indicated by this absolute encoder. The angle is
+     * continuous and so could be negative or above 2*pi.
      */
     public double getAngleRadians(){
         return Math.toRadians(getAngleDegrees());
@@ -60,22 +65,82 @@ public class AnalogAbsoluteEncoder {
      * Returns the angle read from the analog encoder in degrees. This angle is
      * computed based on the voltages given to the constructor of
      * AnalogAbsoluteEncoder.
-     * @return The angle read from the analog encoder in degrees. The angle will
-     * always be in the range 0 - 360.
+     * @return The angle read from the analog encoder in degrees. The angle is
+     * continuous and so could be negative or above 360.0.
      */
     public double getAngleDegrees(){
         double fractionAboveMin = (getVoltage() - minVolts) / (maxVolts - minVolts);
-        double angle = 360.0 * fractionAboveMin;
+        double angle = (360.0 * fractionAboveMin);
         if(reversed){
             angle = 360.0 - angle;
         }
+        if(reverseReadDirection){
+            angle = 360.0 - angle;
+        }
         angle += angleOffset;
-        if(angle < 0.0){
-            angle = 360.0 + angle;
+        //Check wrap around
+        double dist = angle - lastAngle;
+        if(dist > MAX_PRE_READ_DISTANCE){
+            //Current angle jumped too high, decrease it.
+            double difference = Math.ceil(Math.abs(dist) / 360.0) * 360.0;
+            angleOffset -= difference;
+            angle -= difference;
         }
-        if(angle > 360.0){
-            angle = angle - 360.0;
+        if(dist < (-1.0*MAX_PRE_READ_DISTANCE)){
+            //Current angle jumped too high, decrease it.
+            double difference = Math.ceil(Math.abs(dist) / 360.0) * 360.0;
+            angleOffset += difference;
+            angle += difference;
         }
+        lastAngle = angle;
+        angle += zeroingOffset;
         return angle;
+    }
+    
+    /**
+     * This method is used by PIDControllers to read the value of this encoder.
+     * It returns the angle of the encoder in <i>degrees</i>.
+     * This method is an alias for {@link #getAngleDegrees()}.
+     * @return The current angle on the encoder in degrees.
+     * @see #getAngleDegrees()
+     */
+    public double pidGet(){
+        return this.getAngleDegrees();
+    }
+    
+    /**
+     * This method returns the offset used to zero the encoder. This is the
+     * offset that is added to the angle in the final step just prior to
+     * returning.
+     * @return The offset added to the angle to zero the encoder. This is
+     * measured in degrees.
+     */
+    public double getOffset(){
+        return zeroingOffset;
+    }
+    
+    /**
+     * This method sets the offset used by the encoder to zero its angle.
+     * This angle is measured in degrees.
+     * @param offset The new offset to be used by this encoder.
+     */
+    public void setOffset(double offset){
+        this.zeroingOffset = (offset + zeroingOffset);
+    }
+    
+    /**
+     * Convenience method used to zero this encoder. After calling this method
+     * the encoder will read zero.
+     */
+    public void zeroEncoder(){
+        setOffset(-1.0*getAngleDegrees());
+    }
+    
+    /**
+     * Calling this method causes the encoder to compute its current angle and
+     * update its rollover detection.
+     */
+    public void update(){
+        getAngleDegrees();
     }
 }
