aefef6d48b691f811a905572b0f0d220ed352e00
Jeffrey <jeffreylee37@gmail.com>
2014-01-30 16:50:58 -0600

    Added methods to get magnitude and direction of gamepad joysticks.
    These are convenience methods that make GamepadAxisState easier to use in
    conjuction with RobotDrive.

--- src/com/edinarobotics/utils/gamepad/GamepadAxisState.java
+++ src/com/edinarobotics/utils/gamepad/GamepadAxisState.java
@@ -1,6 +1,7 @@
 package com.edinarobotics.utils.gamepad;
 
 import com.edinarobotics.utils.math.Vector2;
+import com.sun.squawk.util.MathUtils;
 
 /**
  * This class represents the state of all joystick axes of a Gamepad.
@@ -36,5 +37,47 @@ public class GamepadAxisState {
      */
     public Vector2 getRightJoystick(){
         return right;
+    }
+    
+    /**
+     * Returns the current magnitude of the Vector2 representing the state of 
+     * the gamepad's left joystick.
+     * @return the current magnitude of the Vector2 representing the state of 
+     * the gamepad's left joystick.
+     */
+    public double getLeftMagnitude() {
+        return getLeftJoystick().magnitude();
+    }
+    
+    /**
+     * Returns the current magnitude of the Vector2 representing the state of 
+     * the gamepad's right joystick.
+     * @return the current magnitude of the Vector2 representing the state of 
+     * the gamepad's right joystick.
+     */
+    public double getRightMagnitude() {
+        return getRightJoystick().magnitude();
+    }
+    
+    /**
+     * Returns the current direction of the Vector2 representing the state of
+     * the gamepad's left joystick in degrees (-180 to 180). This return value
+     * is suitable to be passed into RobotDrive.
+     * @return the current direction of the Vector2 representing the state of
+     * the gamepad's left joystick in degrees (-180 to 180).
+     */
+    public double getLeftDirection() {
+        return Math.toDegrees(MathUtils.atan2(left.getY(), left.getX()));
+    }
+    
+    /**
+     * Returns the current direction of the Vector2 representing the state of
+     * the gamepad's right joystick in degrees (-180 to 180). This return value
+     * is suitable to be passed into RobotDrive.
+     * @return the current direction of the Vector2 representing the state of
+     * the gamepad's right joystick in radians (-180 to 180).
+     */
+    public double getRightDirection() {
+        return Math.toDegrees(MathUtils.atan2(right.getY(), right.getX()));
     }
 }
