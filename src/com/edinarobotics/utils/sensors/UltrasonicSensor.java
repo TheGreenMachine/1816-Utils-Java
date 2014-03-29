7decc28c029cf49d6a48379e288fb789926c1496
Jeffrey Lee <jeffreylee37@gmail.com>
2014-03-28 21:15:22 -0500

    Add basic filtering to UltrasonicSensor.
    The filtering removes the highest and lowest 10% of values and
    then averages what values remain.

--- src/com/edinarobotics/utils/sensors/UltrasonicSensor.java
+++ src/com/edinarobotics/utils/sensors/UltrasonicSensor.java
@@ -1,10 +1,14 @@
 package com.edinarobotics.utils.sensors;
 
+import com.sun.squawk.util.Arrays;
 import edu.wpi.first.wpilibj.AnalogChannel;
 
 public class UltrasonicSensor {
     private AnalogChannel analogChannel;
     private double scale;
+    private int TO_AVERAGE = 35;
+    private int lastValue = 0;
+    private double[] distances = new double[TO_AVERAGE];
     
     private final boolean isScaled;
     
@@ -20,10 +24,31 @@ public class UltrasonicSensor {
     }
     
     public double getDistance() {
+        double toReturn;
         if(isScaled) {
-            return scale * getVoltage();
+            toReturn = scale * getVoltage();
         }
-        return getVoltage();
+        else{
+            toReturn = getVoltage();
+        }
+        lastValue++;
+        if(lastValue >= distances.length){
+            lastValue = 0;
+        }
+        distances[lastValue] = toReturn;
+        return averageArr();
+    }
+    
+    private double averageArr(){
+        double[] workingArray = Arrays.copy(distances, 0, distances.length, 0, distances.length);
+        Arrays.sort(workingArray);
+        int lowIndex = (int)Math.ceil(workingArray.length * 0.1);
+        int highIndex = (int)Math.ceil(workingArray.length * 0.9);
+        double sum = 0;
+        for(int i=lowIndex; i<highIndex; i++){
+            sum += distances[i];
+        }
+        return sum / ((highIndex - lowIndex) * 1.0);
     }
     
     public double getVoltage() {
