88b37c19faffc3703bc943583b743eb8ccead248
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2016-02-24 16:18:57 -0600

    Added the ability to read a Pressure Sensor.

---
+++ src/com/edinarobotics/utils/sensors/PressureSensor.java
@@ -0,0 +1,49 @@
+package com.edinarobotics.utils.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class PressureSensor {
+	
+	private AnalogInput input;
+	private double inputVoltage;
+	
+	private static final double DEFAULT_VOLTAGE = 5.0;
+	private final int SLOPE = 250;
+	private final int Y_INTERCEPT = -25;
+	
+	/**
+	 * Creates a new Pressure Sensor connected to the given analog channel.
+	 * 
+	 * @param input The port that the Pressure Sensor is plugged into. 
+	 * @param inputVoltage The voltage that we are providing to the sensor.
+	 */
+	public PressureSensor(int input, double inputVoltage) {
+		this.input = new AnalogInput(input);
+		this.inputVoltage = inputVoltage;
+	}
+	
+	/**
+	 * Creates a new Pressure Sensor connected to the given analog channel.
+	 * 
+	 * @param input The port that the Pressure Sensor is plugged into.
+	 */
+	public PressureSensor(int input) {
+		this.input = new AnalogInput(input);
+		this.inputVoltage = DEFAULT_VOLTAGE;
+	}
+	
+	/**
+	 * @return The pressure (psi) the sensor is reading.
+	 */
+	public double getPressure() {
+		return  SLOPE * (input.getVoltage()/inputVoltage) + Y_INTERCEPT;
+	}
+	
+	/**
+	 * @return The voltage that the Pressure Sensor is receiving.
+	 */
+	public double getVoltage() {
+		return input.getVoltage();
+	}
+
+}
